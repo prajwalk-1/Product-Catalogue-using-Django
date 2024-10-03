53e3ea11bbe63effa72f6c2c0eb3bc6b446260e1
Prajwal Kanade <142607979+prajwalk-1@users.noreply.github.com>
2024-10-03 12:35:46 +0530

    urls.py

--- product_catalog/urls.py
+++ product_catalog/urls.py
@@ -1,16 +1,10 @@
 
 from django.contrib import admin
-from django.urls import path
-
 from django.conf.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
- 
-from django.contrib import admin
 from django.urls import path, include
 
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
