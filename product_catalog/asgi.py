497790bbd0f8d52693ee498385dc13d4c831f2ca
Prajwal Kanade <142607979+prajwalk-1@users.noreply.github.com>
2024-10-03 12:34:59 +0530

    asgi.py

--- product_catalog/asgi.py
+++ product_catalog/asgi.py
@@ -8,9 +8,7 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "product_catalog.settings")
-
 application = get_asgi_application()
