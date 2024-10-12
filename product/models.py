f39c7e48e6b5048726eb1fdb9679209cdc833f56
Prajwal Kanade <142607979+prajwalk-1@users.noreply.github.com>
2024-10-12 21:51:55 +0530

    models.py

--- product/models.py
+++ product/models.py
@@ -3,7 +3,6 @@ from datetime import datetime
 
 class Category(models.Model):
     name = models.CharField(max_length=100, default='')
-
     def __str__(self):
         return self.name
 
@@ -17,17 +16,14 @@ class Product(models.Model):
     description = models.TextField()
     is_published = models.BooleanField(default=True)
     # created_at = models.DateTimeField(default=datetime.now, blank=True)
-
     def __str__(self):
         return self.name
-    
     
 class Comment(models.Model):
     product = models.ForeignKey(Product, related_name="comments", on_delete=models.CASCADE)
     commenter_name = models.CharField(max_length=200)
     comment_body = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
-
     def __str__(self):
         return '%s - %s' % (self.product.name, self.commenter_name)
 
